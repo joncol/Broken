a9662330a5673db63ea76a5c151ba71c76aa94ee
Jonas Collberg <jonas.collberg@mykolab.com>
2016-10-20 16:06:33 +0200

    Remove obsolete commented out code in Main.hs

--- app/Main.hs
+++ app/Main.hs
@@ -1,30 +1,4 @@
 module Main where
 
--- import Data.Semigroup
--- import Test.Tasty
--- import Test.Tasty.QuickCheck hiding (Failure, Success)
-
--- data BoolConj = BoolConj Bool deriving (Eq, Show)
-
--- instance Semigroup BoolConj where
---   BoolConj True <> BoolConj True = BoolConj True
---   BoolConj _    <> BoolConj _    = BoolConj False
-
--- instance Monoid BoolConj where
---     mempty  = BoolConj True -- <- note the error, should be BoolConj True
---     mappend = (<>)
-
--- instance Arbitrary BoolConj where
---   arbitrary = BoolConj <$> arbitrary
-
--- type IdType a = a -> Bool
-
--- rightId :: (Monoid a, Eq a) => a -> Bool
--- rightId x = x `mappend` mempty == x
-
 main :: IO ()
 main = undefined
--- main = defaultMain $ testGroup "Quickcheck tests"
---   [ testProperty "BoolConj monoid has right identity"
---       (rightId :: IdType BoolConj)
---   ]
